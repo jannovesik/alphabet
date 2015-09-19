68cfe15e94721497175023423e42b71a36d70c71
janno <janno@chanz.com>
2015-09-19 15:48:52 +0300

    First version of chargame

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
-    $('#suggestions .char').transfer(function(e){
+$(document).ready(function () {
+    var game = {
+        init: function(word) {
+            this.$suggestions = $('#suggestions');
+            this.$answers = $('#answers');
+            this.$suggestions.html('');
+            this.$answers.html('');
 
+            var data = [];
+            $(word).each(function (i, char) {
+                if (char != '#') {
+                    data.push({'char': char});
+                }
+            });
+            $('.char-template').tmpl(data).appendTo('#suggestions');
+        }
+    };
+
+    var word = window.location.hash.split('');
+    game.init(word);
+
+    $('#suggestions, #answers').sortable({
+        connectWith: '.char-container',
+        placeholder: 'placeholder'
+    });
+
+    $(window).on('hashchange', function() {
+        var word = window.location.hash.split('');
+        game.init(word);
     });
 });
